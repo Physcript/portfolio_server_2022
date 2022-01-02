c9013ce5b1a46d3c9a2a6cc01bed52ed0561a18d
Physcript <82250688+Physcript@users.noreply.github.com>
2022-01-02 16:04:09 +0800

    fixing

--- src/index.ts
+++ src/index.ts
@@ -22,11 +22,11 @@ app.use(cors(corsOptions))
 
 // corspolicy
 app.use((req,res,next) => {
-  res.setHeader('Access-Control-Allow-Origin','http://localhost:3000')
+  res.setHeader('Access-Control-Allow-Origin','https://batino.netlify.app')
   res.setHeader('Access-Control-Allow-Methods', 'PUT,PATCH,DELETE,GET,POST')
   res.setHeader('Access-Control-Allow-headers','X-Requested-With,Content-Type')
   res.setHeader('Access-Control-Allow-credentials','true')
-  next()
+  next()  
 })
 
 // middleware
